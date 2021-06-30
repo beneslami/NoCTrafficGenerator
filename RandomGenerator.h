6291d6e8f40dc32d09514d7e9bc367a600d80661
Benyamin <beneslami72@gmail.com>
2021-06-30 15:47:45 +0200

    statistical distributions

---
+++ RandomGenerator.h
@@ -0,0 +1,94 @@
+//
+// Created by Ben on 6/30/21.
+//
+
+#ifndef RANDOMGENERATOR_H
+#define RANDOMGENERATOR_H
+
+#include <iostream>
+#include <random>
+#include <map>
+
+namespace RandomGenerator {
+    std::random_device rd;
+    std::mt19937 mt_rng(rd());
+
+    class BernoulliDistribution{
+    public:
+        BernoulliDistribution(double p) {
+            dist = new std::bernoulli_distribution(p);
+        }
+
+        ~BernoulliDistribution() {
+        }
+
+        void SetProbability(double p) {
+            dist = new std::bernoulli_distribution(p);
+        }
+
+        bool Generate() {
+            return (*dist)(mt_rng);
+        }
+
+    private:
+        std::bernoulli_distribution* dist;
+    };
+
+    class UniformDistribution{
+    public:
+        UniformDistribution(int min, int max) {
+            dist = new std::uniform_int_distribution<int>(min, max);
+        }
+
+        ~UniformDistribution() {
+        }
+
+        void SetMinMax(int min, int max) {
+            dist = new std::uniform_int_distribution<int>(min, max);
+        }
+
+        int Generate() {
+            return (*dist)(mt_rng);
+        }
+    private:
+        std::uniform_int_distribution<int>* dist;
+    };
+
+    class NormalDistribution{
+    public:
+        NormalDistribution(float mean, float std){
+            dist = new std::normal_distribution<float>(mean, std);
+        }
+
+        ~NormalDistribution(){
+        }
+
+        float generate(){
+            return (*dist)(mt_rng);
+        }
+
+    private:
+        std::normal_distribution<float>* dist;
+
+    };
+
+    class ExponentialDistribution{
+    public:
+        ExponentialDistribution(double lambda, int intervals){
+            _lambda = lambda;
+            _intervals = intervals;
+        }
+
+        double Generate(){
+            std::exponential_distribution<double> dist(_lambda);
+            return dist(mt_rng);
+        }
+
+    private:
+        double _lambda;
+        int _intervals;
+    };
+};
+
+
+#endif
