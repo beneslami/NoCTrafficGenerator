8c02423e5964ab4771e0924c281c042ba28583b8
Benyamin <beneslami72@gmail.com>
2021-07-03 23:53:52 +0200

    generate source initiator

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 // Created by Ben on 6/30/21.
 //
 
-#include "RandomGenerator.h"
 #include <iostream>
+#include <string>
+#include "TrafficGenerator.h"
+
+unsigned long long numCycles;
 
 int main(int argc, char **argv){
-    if(argc != 4) {
+    if(argc < 2) {
         std::cerr << "Need 2 parameters: [MODEL FILE] [DURATION IN CYCLE]" << std::endl;
         return -1;
     }
-
-    /* 1- open the model file and read argv[1]*/
-
-    //unsigned int numCycles = (int) strtoul(argv[2], NULL, 0);
-
-    /* 2- Run the generator with numCycles */
+    numCycles = std::stoull(argv[2]);
+    TrafficGenerator TGen = TrafficGenerator();
+    TGen.Run();
 
     return 0;
 }
