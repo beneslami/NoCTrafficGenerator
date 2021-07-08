97a3efd1873a7cc5fbd10fbc24a54239576c9f75
Benyamin <beneslami72@gmail.com>
2021-07-08 16:30:29 +0200

    improve

--- Makefile
+++ Makefile
@@ -11,8 +11,12 @@ RandomGenerator.o: RandomGenerator.cpp
 	$(CC) $(CFLAGS) -o RandomGenerator.o RandomGenerator.cpp
 TrafficGenerator.o: TrafficGenerator.cpp
 	$(CC) $(CFLAGS) -o TrafficGenerator.o TrafficGenerator.cpp
-all : main.o MessageType.o PacketQueue.o RandomGenerator.o TrafficGenerator.o
-	$(CC) -o main main.o MessageType.o PacketQueue.o RandomGenerator.o TrafficGenerator.o
+ReadFile.o: ReadFile.cpp
+	$(CC) $(CFLAGS) -o ReadFile.o ReadFile.cpp
+globals.o: globals.cpp
+	$(CC) $(CFLAGS) -o globals.o globals.cpp
+all : main.o MessageType.o PacketQueue.o RandomGenerator.o TrafficGenerator.o ReadFile.o globals.o
+	$(CC) -o main main.o MessageType.o PacketQueue.o RandomGenerator.o TrafficGenerator.o ReadFile.o globals.o
 
 clean:
-	rm -rf main main.o MessageType.o PacketQueue.o RandomGenerator.o TrafficGenerator.o
+	rm -rf main main.o MessageType.o PacketQueue.o RandomGenerator.o TrafficGenerator.o ReadFile.o globals.o
--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <chrono>
 #include "TrafficGenerator.h"
 #include "RandomGenerator.h"
+#include "ReadFile.h"
 
 unsigned long long numCycles;
 int numCores;
@@ -16,14 +18,21 @@ int main(int argc, char **argv){
         std::cerr << "Need 3 parameters: [MODEL FILE] [DURATION IN CYCLE] [NUMBER OF CORE]" << std::endl;
         return -1;
     }
+
+    std::ifstream modelFile(argv[1]);
+    if(!modelFile.good()) {
+        std::cerr << "Could not open file " << argv[1] << std::endl;
+        return -1;
+    }
     numCycles = std::stoull(argv[2]);
     numCores = std::stoi(argv[3]);
+    readModel(modelFile);
     TrafficGenerator TGen = TrafficGenerator();
     auto start = std::chrono::high_resolution_clock::now();
     TGen.Run();
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     std::cout << "duration: " << duration.count() <<"s\n";
-
+    modelFile.close();
     return 0;
 }
