1d59af7d8654a4759441cdb1eb757d7693309251
Benyamin <beneslami72@gmail.com>
2021-07-08 16:29:50 +0200

    add global variables

---
+++ globals.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ben on 7/8/21.
+//
+#include "globals.h"
+
+std::map<double, double>bytes;
+std::map<int, std::map<int, int> > duration;
+std::map<int, int>traffic;
+std::string destin;
---
+++ globals.h
@@ -0,0 +1,16 @@
+//
+// Created by Ben on 7/8/21.
+//
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <map>
+#include <iostream>
+
+extern std::map<double, double>bytes;
+extern std::map<int, std::map<int, int> > duration;
+extern std::map<int, int>traffic;
+extern std::string destin;
+
+#endif
