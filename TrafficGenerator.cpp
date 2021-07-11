a320d8c97789b2d838f31844eb9ef1f4408f038a
Benyamin <beneslami72@gmail.com>
2021-07-12 01:21:02 +0200

    support for multi chiplet nodes

--- TrafficGenerator.cpp
+++ TrafficGenerator.cpp
@@ -10,9 +10,6 @@
 #include <iostream>
 #include <fstream>
 
-extern unsigned long long numCycles;
-extern int numCores;
-int window = 5;
 std::map<int, InjectReqMsg> inTransitPackets;
 std::map<int, struct TrafficGenerator::transaction_t> inTransitTransactions;
 PacketQueue packet_queue;
@@ -91,164 +88,47 @@ void TrafficGenerator::Run() {
     int cycle = 1;
     RandomGenerator::UniformDistribution dst;
     if(!destin.compare("uniform")){
-        RandomGenerator::UniformDistribution dst = RandomGenerator::UniformDistribution(0, numCores-1);
+        dst = RandomGenerator::UniformDistribution(0, numCores-1);
     }
     else if(!destin.compare("normal")){
-        RandomGenerator::UniformDistribution dst = RandomGenerator::UniformDistribution(0, numCores-1);
+        dst = RandomGenerator::UniformDistribution(0, numCores-1);
     }
 
     RandomGenerator::CustomDistribution byte = RandomGenerator::CustomDistribution(bytes);
-    byte.show();
 
-    /*
+    std::map<int, RandomGenerator::CustomDistribution> inter_arrival;
+    std::map<int, std::map<double, double> >::iterator it;
+    for(it = duration.begin(); it != duration.end(); ++it){
+        RandomGenerator::CustomDistribution temp = RandomGenerator::CustomDistribution(it->second);
+        inter_arrival.insert(std::pair<int, RandomGenerator::CustomDistribution>(it->first, temp));
+    }
+    /*std::map<int, RandomGenerator::CustomDistribution>::iterator i;
+    for(i = inter_arrival.begin(); i != inter_arrival.end(); ++i){
+        std::cout << i->first << std::endl;
+    }*/
 
-
-    b.clear();
-    b.insert(std::pair<double, double>(1, 76420));
-    b.insert(std::pair<double, double>(2, 18823));
-    b.insert(std::pair<double, double>(3, 8601));
-    b.insert(std::pair<double, double>(4, 4796));
-    b.insert(std::pair<double, double>(5, 2930));
-    b.insert(std::pair<double, double>(6, 1626));
-    b.insert(std::pair<double, double>(7, 903));
-    b.insert(std::pair<double, double>(8, 484));
-    b.insert(std::pair<double, double>(9, 227));
-    b.insert(std::pair<double, double>(10, 152));
-    b.insert(std::pair<double, double>(11, 69));
-    b.insert(std::pair<double, double>(12, 32));
-    b.insert(std::pair<double, double>(13, 16));
-    b.insert(std::pair<double, double>(14, 13));
-    b.insert(std::pair<double, double>(15, 8));
-    b.insert(std::pair<double, double>(16, 6));
-    b.insert(std::pair<double, double>(17, 1));
-    b.insert(std::pair<double, double>(18, 1));
-    b.insert(std::pair<double, double>(20, 1));
-    b.insert(std::pair<double, double>(24, 1));
-    b.insert(std::pair<double, double>(32, 1));
-    b.insert(std::pair<double, double>(54, 1));
-    b.insert(std::pair<double, double>(94, 1));
-    b.insert(std::pair<double, double>(95, 1));
-    b.insert(std::pair<double, double>(249, 1));
-    b.insert(std::pair<double, double>(250, 1));
-    b.insert(std::pair<double, double>(522, 1));
-    RandomGenerator::CustomDistribution byte_0 = RandomGenerator::CustomDistribution(b);
-
-    b.clear();
-    b.insert(std::pair<double, double>(1, 5966));
-    b.insert(std::pair<double, double>(2, 1332));
-    b.insert(std::pair<double, double>(3, 746));
-    b.insert(std::pair<double, double>(4, 434));
-    b.insert(std::pair<double, double>(5, 262));
-    b.insert(std::pair<double, double>(6, 158));
-    b.insert(std::pair<double, double>(7, 126));
-    b.insert(std::pair<double, double>(8, 73));
-    b.insert(std::pair<double, double>(9, 55));
-    b.insert(std::pair<double, double>(10, 40));
-    b.insert(std::pair<double, double>(11, 37));
-    b.insert(std::pair<double, double>(12, 19));
-    b.insert(std::pair<double, double>(13, 26));
-    b.insert(std::pair<double, double>(14, 14));
-    b.insert(std::pair<double, double>(15, 11));
-    b.insert(std::pair<double, double>(16, 10));
-    b.insert(std::pair<double, double>(17, 4));
-    b.insert(std::pair<double, double>(18, 5));
-    b.insert(std::pair<double, double>(19, 6));
-    b.insert(std::pair<double, double>(21, 4));
-    b.insert(std::pair<double, double>(22, 2));
-    b.insert(std::pair<double, double>(23, 2));
-    b.insert(std::pair<double, double>(24, 2));
-    b.insert(std::pair<double, double>(25, 2));
-    b.insert(std::pair<double, double>(26, 4));
-    b.insert(std::pair<double, double>(27, 2));
-    b.insert(std::pair<double, double>(28, 1));
-    b.insert(std::pair<double, double>(32, 1));
-    b.insert(std::pair<double, double>(33, 1));
-    b.insert(std::pair<double, double>(35, 1));
-    b.insert(std::pair<double, double>(143, 1));
-    b.insert(std::pair<double, double>(169, 1));
-    b.insert(std::pair<double, double>(332, 1));
-    RandomGenerator::CustomDistribution byte_8 = RandomGenerator::CustomDistribution(b);
-
-    b.clear();
-    b.insert(std::pair<double, double>(1, 18053));
-    b.insert(std::pair<double, double>(2, 9499));
-    b.insert(std::pair<double, double>(3, 5742));
-    b.insert(std::pair<double, double>(4, 3398));
-    b.insert(std::pair<double, double>(5, 1904));
-    b.insert(std::pair<double, double>(6, 1116));
-    b.insert(std::pair<double, double>(7, 602));
-    b.insert(std::pair<double, double>(8, 328));
-    b.insert(std::pair<double, double>(9, 210));
-    b.insert(std::pair<double, double>(10, 81));
-    b.insert(std::pair<double, double>(11, 57));
-    b.insert(std::pair<double, double>(12, 31));
-    b.insert(std::pair<double, double>(13, 14));
-    b.insert(std::pair<double, double>(14, 3));
-    b.insert(std::pair<double, double>(15, 3));
-    b.insert(std::pair<double, double>(16, 2));
-    b.insert(std::pair<double, double>(17, 1));
-    b.insert(std::pair<double, double>(18, 2));
-    b.insert(std::pair<double, double>(25, 1));
-    b.insert(std::pair<double, double>(38, 1));
-    b.insert(std::pair<double, double>(45, 1));
-    b.insert(std::pair<double, double>(62, 1));
-    b.insert(std::pair<double, double>(63, 9));
-    b.insert(std::pair<double, double>(64, 8));
-    b.insert(std::pair<double, double>(70, 1));
-    b.insert(std::pair<double, double>(101, 1));
-    RandomGenerator::CustomDistribution byte_136 = RandomGenerator::CustomDistribution(b);
-    */
-    /*
     std::map<int, int>traffic;
     std::map<int, int>::iterator pointer;
     while(cycle < numCycles){
         int threshold;
-        for (int src = 0; src < numCores; src++){
+        for (int src = 0; src < numCores; src++) {
             int source = src;
             int destination = dst.Generate();
-            while(source == destination){
+            while (source == destination) {
                 destination = dst.Generate();
             }
             if (destination != src) {
                 int byteInject = byte.Generate();
-                if(byteInject == 0){
-                    threshold = byte_0.Generate();
-                    int i = 0;
-                    while(i < threshold){
-                        if(traffic.find(cycle + i) != traffic.end()){
-                            traffic[cycle+i] += byteInject;
-                        }
-                        else{
-                            traffic[cycle+i] = byteInject;
-                        }
-                        i++;
+                threshold = inter_arrival.at(byteInject).Generate();
+                int i = 0;
+                while(i < threshold){
+                    if(traffic.find(cycle + i) != traffic.end()){
+                        traffic[cycle + i] += byteInject;
                     }
-                }
-                else if(byteInject == 8){
-                    threshold = byte_8.Generate();
-                    int i = 0;
-                    while(i < threshold){
-                        if(traffic.find(cycle + i) != traffic.end()){
-                            traffic[cycle+i] += byteInject;
-                        }
-                        else{
-                            traffic[cycle+i] = byteInject;
-                        }
-                        i++;
+                    else{
+                        traffic[cycle +i ] = byteInject;
                     }
-                }
-                else if(byteInject == 136){
-                    threshold = byte_136.Generate();
-                    int i = 0;
-                    while(i < threshold){
-                        if(traffic.find(cycle + i) != traffic.end()){
-                            traffic[cycle + i] += byteInject;
-                        }
-                        else{
-                            traffic[cycle + i] = byteInject;
-                        }
-                        i++;
-                    }
+                    i++;
                 }
             }
         }
@@ -256,8 +136,9 @@ void TrafficGenerator::Run() {
     }
 
     for(pointer = traffic.begin(); pointer != traffic.end(); ++pointer){
-        myfile << pointer->first << "," << pointer->second << std::endl;
-    }*/
+       myfile << pointer->first << "," << pointer->second << std::endl;
+   }
+
     myfile.close();
 }
 
