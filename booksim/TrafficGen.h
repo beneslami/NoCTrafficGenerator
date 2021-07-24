433212370c1754fbf9b92f7871a067f6ee653635
Benyamin <beneslami72@gmail.com>
2021-07-25 00:26:46 +0200

    code add

--- booksim/TrafficGen.h
+++ booksim/TrafficGen.h
@@ -9,7 +9,6 @@
 #include "src/flit.hpp"
 #include "src/stats.hpp"
 #include "src/module.hpp"
-#include "src/network.hpp"
 #include "src/traffic.hpp"
 #include "src/routefunc.hpp"
 #include "src/outputset.hpp"
@@ -19,15 +18,16 @@
 #include "src/trafficmanager.hpp"
 #include "src/networks/network.hpp"
 
-class trafficGenerator : public TrafficManager{
+class TrafficGen : public TrafficManager{
 private:
+    TrafficGen(const Configuration &config, const vector<Network *> &net);
+
     struct PayLoad {
         int id;
         int subnetwork;
     };
     int  _flit_width;
     int _ideal_interconnect;
-    TraceGenerator *_time_trace;
 
 protected:
     Interface *_interface;
@@ -39,8 +39,9 @@ protected:
     void _GeneratePacket(int source, int size, int cl, int time, int id, int subnetwork, int destination);
 
 public:
-    TrafficManager(const Configuration &config, const vector<BSNetwork *> & net);
-    virtual ~TrafficManager();
+    TrafficGen(const Configuration &config1, const vector<Network *> &net1, const Configuration &config,
+               const vector<Network *> &net);
+    virtual ~TrafficGen();
 };
 
 
--- booksim/trafficGenerator.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Ben on 7/19/21.
-//
-
-#include "trafficGenerator.h"
