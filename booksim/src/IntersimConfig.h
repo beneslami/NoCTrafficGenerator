b5a787e01afdb4bfa41f68c1c4d83c31db7ccc93
Benyamin <beneslami72@gmail.com>
2021-08-03 01:11:46 +0200

    add this for configuration

---
+++ booksim/src/IntersimConfig.h
@@ -0,0 +1,17 @@
+//
+// Created by Ben on 8/1/21.
+//
+
+#ifndef INTERSIMCONFIG_H
+#define INTERSIMCONFIG_H
+
+#include "config_utils.hpp"
+#include "booksim_config.hpp"
+
+class IntersimConfig : public BookSimConfig {
+public:
+    IntersimConfig();
+};
+
+
+#endif
