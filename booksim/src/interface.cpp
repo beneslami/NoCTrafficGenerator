99826999c4201d9fa059cdfc31497a9125a0131b
Benyamin <beneslami72@gmail.com>
2021-08-09 21:45:44 +0200

    bug fix

--- booksim/src/interface.cpp
+++ booksim/src/interface.cpp
@@ -175,10 +175,10 @@ void Interface::push(unsigned input_deviceID, unsigned output_deviceID, void *da
     }
     Flit::FlitType type;
     switch(packet_type){
-        switch 0:   type = Flit::READ_REQUEST;  break;
-        switch 1:   type = Flit::READ_REPLY;    break;
-        switch 2:   type = Flit::WRITE_REQUEST; break;
-        switch 3:   type = Flit::WRITE_REPLY;   break;
+        case 0:   type = Flit::READ_REQUEST;  break;
+        case 1:   type = Flit::READ_REPLY;    break;
+        case 2:   type = Flit::WRITE_REQUEST; break;
+        case 3:   type = Flit::WRITE_REPLY;   break;
         default:    type = Flit::ANY_TYPE;      break;
     }
     _traffic_manager->_GeneratePacket( input_icntID, 0, _traffic_manager->_time, subnet, n_flits, packet_type, data, output_icntID);
